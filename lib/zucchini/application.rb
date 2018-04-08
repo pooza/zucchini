3e88f64a288f807ab8effb152de2f590918cd73a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-04-08 16:34:19 +0900

    #1

--- lib/zucchini/application.rb
+++ lib/zucchini/application.rb
@@ -1,77 +1,39 @@
 require 'sinatra'
 require 'active_support'
 require 'active_support/core_ext'
-require 'zucchini/slack'
 require 'zucchini/config'
-require 'zucchini/html'
 require 'zucchini/package'
-require 'zucchini/logger'
 
 module Zucchini
   class Application < Sinatra::Base
+    set :public, File.join(ROOT_DIR, 'public')
+    set :views, File.join(ROOT_DIR, 'views')
+
     def initialize
       super
       @config = Config.instance
-      @logger = Logger.new(Package.name)
-      @slack = Slack.new if @config['local']['slack']
-      @logger.info({
-        message: 'starting...',
-        package: {
-          name: Package.name,
-          version: Package.version,
-          url: Package.url,
-        },
-        server: {
-          port: @config['thin']['port'],
-        },
-      })
     end
 
-    before do
-      @message = {request:{path: request.path, params:params}, response:{}}
-      @renderer = HTML.new
-    end
-
-    after do
-      @message[:response][:status] ||= @renderer.status
-      if (@renderer.status < 400)
-        @logger.info(@message)
-      else
-        @logger.error(@message)
-      end
-      status @renderer.status
-      content_type @renderer.type
-    end
-
-    ['/', '/index'].each do |route|
-      get route do
-        @renderer.template_file = 'index.erb'
-        movies = []
-        @config['application']['suffixes'].each do |suffix|
-          Dir.glob(File.join(@config['local']['dir'], "*#{suffix}")).each do |f|
-            movies.push(f)
-          end
+    get '/' do
+      @package = Package.to_h
+      @movies = []
+      @config['application']['suffixes'].each do |suffix|
+        Dir.glob(File.join(ROOT_DIR, "public/movie/*#{suffix}")).each do |f|
+          f.sub!(File.join(ROOT_DIR, 'public'), '')
+          @movies.push(f)
         end
-        @renderer.vars['movies'] = movies
-        return @renderer.to_s
       end
+      erb :index
     end
 
     not_found do
-      @renderer.status = 404
-      @message[:response][:message] = "Resource #{@message[:request][:path]} not found."
-      @renderer.template_file = 'not_found.erb'
-      @renderer.vars[:message] = @message
-      return @renderer.to_s
+      status 404
+      "Not found."
     end
 
     error do
-      @renderer.status = 500
-      @message[:response][:message] = env['sinatra.error'].message
-      @renderer.template_file = 'error.erb'
-      @renderer.vars[:message] = @message
-      @slack.say(@message) if @slack
-      return @renderer.to_s
+      status 500
+      env['sinatra.error'].message
     end
   end
 end
--- lib/zucchini/package.rb
+++ lib/zucchini/package.rb
@@ -17,5 +17,14 @@ module Zucchini
     def self.full_name
       return "#{self.name} #{self.version}"
     end
+
+    def self.to_h
+      return {
+        name: self.name,
+        version: self.version,
+        url: self.url,
+        full_name: self.full_name,
+      }
+    end
   end
 end
